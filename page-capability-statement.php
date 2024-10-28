1b174a5aabea698cf77ae87040b1437fa7f995a4
SamuvelRaja <josephsamuvel001@gmail.com>
2024-10-28 08:14:17 +0530

    added capablity Statement

---
+++ page-capability-statement.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Template Name: Capability Statement
+*/
+get_header(); 
+?>
+
+<style>
+    .responsive-img {
+        max-width: 100%;
+        height: auto;
+    }
+    .content{
+        width: fit-content;
+        margin: 80px auto;
+    }
+    
+</style>
+
+<div class="content">
+    <img src="<?php echo get_template_directory_uri(); ?>/asset/images/2capabilityStatement.webp" alt="Capability Statement" class="responsive-img">
+</div>
+
+<?php get_footer(); ?>
